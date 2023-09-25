f49791fbc9103b3363f717afd0e775acd00cea39
wesrer <sayakchatterjee1309@gmail.com>
2023-09-25 04:54:49 -0400

    adding git local overrides with ssh key reloads

--- fish/wsl.config.fish
+++ fish/wsl.config.fish
@@ -9,6 +9,9 @@ fish_ssh_agent
 # Reload config from the ln linked file in fish
 alias reloadconf="source ~/.config/fish/config.fish"
 
+# View current conf
+alias viewconf="bat ~/.config/fish/config.fish"
+
 # Size of current dir in human readable format
 alias dirsize="du -sh"
 
@@ -24,21 +27,27 @@ alias fmt-toml="taplo format"
 # Format rust files with +nightly
 alias fmt-rust="cargo +nightly fmt"
 
+# Reset author of last commit in git
+alias git-reauthor="git commit --amend --reset-author"
+
+# Amend your last commit
+alias git-re="git commit --amend"
+
 # WSL2 aliases
 # Go to your wiki directory
 alias cdwiki="cd /mnt/c/Users/takas/Documents/sadboiwiki"
 
 # Setup Astira dev
-alias assdev="ssh-add -D && ssh-add ~/.ssh/astriaTM"
+alias assdev="git config user.name "dereksione" && git config user.email "derek@astria.org" && ssh-add -D && ssh-add ~/.ssh/astriaTM"
 
 # Setup TM dev
-alias tmdev="ssh-add -D && ssh-add ~/.ssh/id_sojudrinkerTM"
+alias tmdev="git config user.name "soju-drinker" && git config user.email "takashi.hatake@outlook.com" && ssh-add -D && ssh-add ~/.ssh/id_sojudrinkerTM"
 
 # Set dev under wes_xyz (giraffe)
-alias giraffedev="ssh-add -D && ssh-add ~/.ssh/wes_station"
+alias giraffedev="git config user.name "wes_xyz" && git config user.email "wesrer@skiff.com" && ssh-add -D && ssh-add ~/.ssh/wes_stationewesrer_TM"
 
 # Setup dev under my normal repository
-alias wesrerdev="ssh-add -D && ssh-add ~/.ssh/wesrer_TM"
+alias wesrerdev="git config user.name "wesrer" && git config user.email "sayakchatterjee1309@gmail.com" && ssh-add -D && ssh-add ~/.ssh/wesrer_TM"
 
 # Created by `pipx` on 2023-05-04 18:49:14
 set PATH $PATH /home/sojudrinker/.local/bin
