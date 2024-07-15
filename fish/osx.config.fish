8efea3fcdf1e76458112edf24b62b73f2b570413
dereksione <dereksione@skiff.com>
2024-07-15 15:50:27 -0400

    feat(fish-config): adding more aliases for everyday tasks for work computer

--- fish/osx.config.fish
+++ fish/osx.config.fish
@@ -1,4 +1,8 @@
 alias el="eza -la"
 alias e="eza"
 alias reload="source ~/.config/fish/config.fish"
-
+alias tis="timew summary :ids"
+alias editconfig="vim ~/Code/.dotfiles/fish/osx.config.fish"
+alias tbull="task project:bullpen"
+alias derek="ssh-add ~/.ssh/dereksione_gh"
+alias wes="ssh-add ~/.ssh/wesrer_gh"
