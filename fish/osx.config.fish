a7c92a9652f9f202949e273c7d022b8e2b09af6b
dereksione <dereksione@skiff.com>
2024-07-19 03:43:42 -0400

    More comments and better organization

--- fish/osx.config.fish
+++ fish/osx.config.fish
@@ -1,19 +1,37 @@
 alias el="eza -la"
 alias e="eza"
 alias c="clear"
-alias cr="clear && reload"
-alias reload="source ~/.config/fish/config.fish"
-alias tis="timew summary :ids"
+
+# --------------------------------------------------------
+# Dotfiles management
+# --------------------------------------------------------
+
 alias editconfig="vim ~/Code/.dotfiles/fish/osx.config.fish"
 alias cdconfig="cd ~/Code/.dotfiles/"
+alias reload="source ~/.config/fish/config.fish"
+alias cr="clear && reload"
+
+# --------------------------------------------------------
+# Personal dev setup
+# --------------------------------------------------------
+
 alias derek="ssh-add ~/.ssh/dereksione_gh"
 alias wes="ssh-add ~/.ssh/wesrer_gh"
+
+alias update="brew update & brew upgrade"
+
+# --------------------------------------------------------
+# Rust specific commands
+# --------------------------------------------------------
+
 alias carb="cargo build"
 alias carr="cargo run"
 alias cart="cargo test"
-alias update="brew update & brew upgrade"
 
+# --------------------------------------------------------
 # Git specific commands
+# --------------------------------------------------------
+
 alias gs="git status"
 alias ga="git add"
 alias gc="git commit -m"
@@ -26,9 +44,15 @@ alias gfa="git fetch --all"
 alias gl="git log"
 
 # Combinations
+
 alias gcf="gaa && gca && gpf"
 
+# --------------------------------------------------------
+# Taskwarrior specific 
+# --------------------------------------------------------
+
 # Bullpen specific commands
+
 alias tbull="task project:bullpen"
 alias tbulla="task add project:bullpen"
 alias tbullx="task project:bullpen complete"
@@ -38,3 +62,25 @@ alias thabx="task project:habits complete"
 alias tch="task project:chores"
 alias tcha="task project:chores add"
 alias tchx="task project:chores complete"
+
+# --------------------------------------------------------
+# Taskwarrior specific 
+# --------------------------------------------------------
+
+alias tis="timew summary :ids"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
